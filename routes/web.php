efa1590399ef3cbd9e94ce6a5a4311ada0dce037
Mihir Manoj Lakhamje <lakhamjemihir@gmail.com>
2025-11-12 02:45:51 +0530

    Add user management routes for CRUD operations

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ForgetPasswordController;
 use App\Http\Controllers\ResetPasswordController;
 use App\Http\Controllers\SessionController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [SessionController::class, 'loginPage'])->name('login');
@@ -20,4 +21,13 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/logout', [SessionController::class, 'logout'])->name('logout');
 
     Route::get('/dashboard', [DashboardController::class, '__invoke'])->name('dashboard');
+
+    Route::prefix('users')->group(function () {
+        Route::get('/', [UserController::class, 'index'])->name('users.index');
+        Route::get('/create', [UserController::class, 'create'])->name('users.create');
+        Route::get('/{user}/edit', [UserController::class, 'edit'])->name('users.edit');
+        Route::patch('/{user}', [UserController::class, 'update'])->name('users.update');
+        Route::post('/', [UserController::class, 'store'])->name('users.store');
+        Route::delete('/{user}', [UserController::class, 'destroy'])->name('users.destroy');
+    });
 });
