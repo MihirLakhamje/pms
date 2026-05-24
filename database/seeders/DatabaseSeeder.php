3ac1cdef5295efe278cc317fc5824d5fd4839db0
Mihir Lakhamje <lakhamjemihir@gmail.com>
2026-05-24 21:13:39 +0530

    add factories for Attachment and TaskReview models; update DatabaseSeeder with sample data for users and projects

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Project;
+use App\Models\Task;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -19,6 +21,7 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
+            'id' => 1,
             'name' => 'admin',
             'email' => 'admin@gmail.com',
             'password' => Hash::make('123456'),
@@ -26,6 +29,7 @@ class DatabaseSeeder extends Seeder
         ]);
 
         User::factory()->create([
+            'id' => 2,
             'name' => 'manager',
             'email' => 'pm@gmail.com',
             'password' => Hash::make('123456'),
@@ -33,10 +37,60 @@ class DatabaseSeeder extends Seeder
         ]);
 
         User::factory()->create([
+            'id' => 3,
             'name' => 'employee',
             'email' => 'emp@gmail.com',
             'password' => Hash::make('123456'),
             'role' => 'employee',
+        ]);
+
+        $project1 = Project::factory()->create([
+            'id' => 1,
+            'name' => 'Kokan Soundarya',
+            'description' => 'Kokan Soundarya is a homestay service provider in the Konkan region of Maharashtra, India. They needs a brand site to showcase their services, attract customers, and provide information about their offerings.',
+            'status' => 'in_progress',
+            'deadline' => now()->addDays(30),
+        ]);
+
+        $project2 = Project::factory()->create([
+            'id' => 2,
+            'name' => 'Art life studio',
+            'description' => 'Art Life Studio is an art gallery and studio that needs a website to showcase their artists, exhibitions, and art classes. The website should provide information about the studio, upcoming events, and allow visitors to contact the studio for inquiries.',
+            'status' => 'in_progress',
+            'deadline' => now()->addDays(30),
+        ]);
+
+        $project1->users()->attach([2, 3]);
+        $project2->users()->attach([2, 3]);
+
+        Task::factory()->create([
+            'id' => 1,
+            'project_id' => 1,
+            'title' => 'Design Homepage',
+            'description' => 'Create a visually appealing homepage that highlights the key features of Kokan Soundarya and encourages visitors to explore the site.',
+            'status' => 'in_progress',
+            'assignee_id' => 3,
+        ], [
+            'id' => 2,
+            'project_id' => 1,
+            'title' => 'Develop Booking System',
+            'description' => 'Implement a booking system that allows customers to check availability, make reservations, and receive confirmation emails.',
+            'status' => 'in_progress',
+            'assignee_id' => 3,
+        ], [
+            'id' => 3,
+            'project_id' => 2,
+            'title' => 'Create Artist Profiles',
+            'description' => 'Develop individual artist profile pages that showcase their work, biography, and upcoming exhibitions.',
+            'status' => 'in_progress',
+            'assignee_id' => 3,
+        ], [
+            'id' => 4,
+            'project_id' => 2,
+            'title' => 'Set Up Contact Form',
+            'description' => 'Implement a contact form that allows visitors to send inquiries about art classes and exhibitions.',
+            'status' => 'in_progress',
+            'assignee_id' => 3,
         ]);
     }
 }
